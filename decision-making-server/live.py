1c1b1a8b0a4a5f90efa86432fe8dcc950cbee2e7
Dan Silver <dannysilver3@gmail.com>
2015-05-25 22:04:57 -0700

    Removed extra imports

--- decision-making-server/live.py
+++ decision-making-server/live.py
@@ -1,17 +1,8 @@
 # This server gets pings to localhost:5000/ with sensor data and responds with the
 # prediction of a collision occurring.
 
-import numpy as np
-from numpy import cumsum as sum_split
-from sklearn.metrics import accuracy_score
-from sklearn.svm import SVC
-from StringIO import StringIO
-import pdb
+from flask import Flask, request
 import cPickle
-
-from datetime import timedelta
-from flask import Flask, make_response, request, current_app
-from functools import update_wrapper
 
 app = Flask(__name__)
 
